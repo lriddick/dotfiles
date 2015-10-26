669c017476cb341298785306f49163e0285d34ab
lriddick <lauren.riddick@techblur.com>
2015-10-26 09:21:24 +0000

    Init my vimrc,tmux.conf, and script mono and azure-cli tools

---
+++ azure/install.sh
@@ -0,0 +1,1 @@
+npm install azure-cli -g
---
+++ mono/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Get Mono
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install mono-devel
+sudo apt-get install mono-complete
+
+# Get libuv
+
+sudo apt-get install automake libtool curl
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
+
+# DVM
+sudo apt-get install unzip
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
